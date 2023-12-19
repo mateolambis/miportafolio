ab383041d75c4a9916e74cfc0c97542b6e07ceee
mateolambis <mateolambisnacional@gmail.com>
2023-12-19 17:47:50 -0500

    actualizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["" ,"Web Designer", "web Developer", "Systems Technician"],
+    strings: ["" ,"Diseñador web", "Desarrollador web", "Tecnico de sistemas"],
     typeSpeed: 100,
     backSpeed: 60,
     loop:true
